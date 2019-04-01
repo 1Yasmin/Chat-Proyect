9cc6235bf38098defbf1dd52bdbf2eab07f63d54
ram16230 <ram16230@uvg.edu.gt>
2019-04-01 16:40:51 -0600

    quitar el read en main

--- client.cpp
+++ client.cpp
@@ -170,10 +170,6 @@ int main(int argc, char *argv[]) {
 			if (n < 0)
 			    error("Error en escritura");
 			bzero(buffer, 1024);
-			n = read(sockfd, buffer, 1024);
-		  	if (n < 0)
-			    error("Error al leer");
-			printf("Server: %s",buffer);
 		}
 		// Ayuda
 		else if(select == 6){
